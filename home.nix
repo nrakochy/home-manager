bceb776bac0fd81cf54e4bd70dc55e65a2dc2b97
Nick Rakochy <nrakochy@anduril.com>
2024-07-17 10:51:42 -0500

    Init

--- home.nix
+++ home.nix
@@ -1,6 +1,6 @@
 { username, homeDirectory, ... }:
 {
-  homeStateVersion = "24.05";
+  home.stateVersion = "24.05";
   home.username = username;
   home.homeDirectory = homeDirectory;
   nixpkgs.config = {
