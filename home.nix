8fac8cbad961ede13da54166d7e47a0f176044b2
Nick Rakochy <nrakochy@anduril.com>
2024-10-21 09:10:50 -0500

    Enable direnv

--- home.nix
+++ home.nix
@@ -3,6 +3,8 @@
   home.stateVersion = "24.05";
   home.username = username;
   home.homeDirectory = homeDirectory;
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
   nixpkgs.config = {
     allowUnfree = true;
   };
